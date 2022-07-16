36d60195b8f5b9811b5ac7fb773ec0d0a616627d
efesoa <molingeefesoa@gmail.com>
2022-07-16 11:21:45 +0200

    Displaying iris dataset

--- pages/Similarity.tsx
+++ pages/Similarity.tsx
@@ -1,9 +1,10 @@
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
-import {Box, Button, FormControl, Grid, TextField, Typography} from "@mui/material";
-import {useState} from "react";
+import {Box, Button, Grid, Table, TextField, Typography} from "@mui/material";
+import {useRef, useState} from "react";
 import axios from "axios";
 import Footer from "../components/Footer";
+import Dataset from "../components/Dataset";
 
 export default function Similarity() {
     const [data, setData] = useState({
@@ -18,27 +19,35 @@ export default function Similarity() {
     });
 
     const handleChange = (e) => {
-        const value = e.target.value;
+        const value = parseFloat(e.target.value);
         setData({
             ...data,
             [e.target.name]: value
         });
     };
 
+    const formClear = useRef();
+    const handleReset = e => {
+        e.preventDefault();
+        formClear.current.reset();
+        setData(
+            {
+                sepal_length: null,
+                sepal_width: null,
+                petal_length: null,
+                petal_width: null,
+                sec_sepal_length: null,
+                sec_sepal_width: null,
+                sec_petal_length: null,
+                sec_petal_width: null
+            }
+        );
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const userData = {
-            sepal_length: data.sepal_length,
-            sepal_width: data.sepal_width,
-            petal_length: data.petal_length,
-            petal_width: data.petal_width,
-            sec_sepal_length: data.sec_sepal_length,
-            sec_sepal_width: data.sec_sepal_width,
-            sec_petal_length: data.sec_petal_length,
-            sec_petal_width: data.sec_petal_width
-        };
         await axios
-            .post("http://127.0.0.1:8000/measurements", userData)
+            .post(`http://127.0.0.1:8000/measurements`, data)
             .then((response) => {
                 console.log(response);
             })
@@ -54,6 +63,15 @@ export default function Similarity() {
             });
     };
 
+    const [dataset, getDataset] = useState(null)
+    const showDataset = () => {
+        axios.get('http://127.0.0.1:8000/dataset')
+            .then((response) => {
+                const irisset = response.data;
+                getDataset(irisset);
+            })
+    }
+
     return (
         <div className={styles.container}>
             <Navbar/><br/>
@@ -62,72 +80,96 @@ export default function Similarity() {
                 Specify Two Flowers
             </Typography>
 
-            <Grid container spacing={2}
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{ my: 3 }}
-            >
-                <form onSubmit={handleSubmit} method="post">
-                    <Grid item>
-                        <Box boxShadow={7} sx={{ p:2 }} borderRadius={5}>
-                            <Typography variant="h5" align='center'>
-                                First Flower
-                            </Typography>
-                            <p>
-                                <label>Sepal Length</label><br/>
-                                <TextField type='number' name='sepal_length' value={data.sepal_length} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Sepal Width</label><br/>
-                                <TextField type='number' name='sepal_width' value={data.sepal_width} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Petal Length</label><br/>
-                                <TextField type='number' name='petal_length' value={data.petal_length} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Petal Width</label><br/>
-                                <TextField type='number' name='petal_width' value={data.petal_width} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                        </Box>
-                    </Grid>
+            <form onSubmit={handleSubmit} method="post" ref={formClear}>
+                <Grid container spacing={2}
+                      direction="row"
+                      justifyContent="center"
+                      alignItems="center"
+                      sx={{ my: 3 }}
+                >
+                        <Grid item>
+                            <Box boxShadow={7} sx={{ p:2 }} borderRadius={5}>
+                                <Typography variant="h5" align='center'>
+                                    First Flower
+                                </Typography>
+                                <p>
+                                    <label>Sepal Length</label><br/>
+                                    <TextField type='number' name='sepal_length' value={data.sepal_length} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }} />
+                                </p>
+                                <p>
+                                    <label>Sepal Width</label><br/>
+                                    <TextField type='number' name='sepal_width' value={data.sepal_width} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                                <p>
+                                    <label>Petal Length</label><br/>
+                                    <TextField type='number' name='petal_length' value={data.petal_length} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                                <p>
+                                    <label>Petal Width</label><br/>
+                                    <TextField type='number' name='petal_width' value={data.petal_width} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                            </Box>
+                        </Grid>
 
-                    <Grid item>
-                        <Box boxShadow={7} sx={{ p:2 }} borderRadius={5}>
-                            <Typography variant="h5" align='center'>
-                                Second Flower
-                            </Typography>
-                            <p>
-                                <label>Sepal Length</label><br/>
-                                <TextField type='number' name='sec_sepal_length' value={data.sec_sepal_length} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Sepal Width</label><br/>
-                                <TextField type='number' name='sec_sepal_width' value={data.sec_sepal_width} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Petal Length</label><br/>
-                                <TextField type='number' name='sec_petal_length' value={data.sec_petal_length} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                            <p>
-                                <label>Petal Width</label><br/>
-                                <TextField type='number' name='sec_petal_width' value={data.sec_petal_width} size='small'
-                                           sx={{ width: '50%' }} onChange={handleChange}/>
-                            </p>
-                        </Box>
-                    </Grid>
+                        <Grid item>
+                            <Box boxShadow={7} sx={{ p:2 }} borderRadius={5}>
+                                <Typography variant="h5" align='center'>
+                                    Second Flower
+                                </Typography>
+                                <p>
+                                    <label>Sepal Length</label><br/>
+                                    <TextField type='number' name='sec_sepal_length' value={data.sec_sepal_length} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                                <p>
+                                    <label>Sepal Width</label><br/>
+                                    <TextField type='number' name='sec_sepal_width' value={data.sec_sepal_width} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                                <p>
+                                    <label>Petal Length</label><br/>
+                                    <TextField type='number' name='sec_petal_length' value={data.sec_petal_length} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                                <p>
+                                    <label>Petal Width</label><br/>
+                                    <TextField type='number' name='sec_petal_width' value={data.sec_petal_width} size='small'
+                                               sx={{ width: '50%' }} onChange={handleChange}
+                                               inputProps={{ min: 0.1, step: 0.1, max: 10, required: 'true' }}/>
+                                </p>
+                            </Box>
+                        </Grid>
 
-                    <Button type="submit" variant="contained">Compute</Button>
-                </form>
-            </Grid>
+                </Grid>
+                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                    <Grid item>
+                        <Button type="submit" variant="contained">Compute</Button>
+                    </Grid>
+                    <Grid item>
+                        <Button type="reset" variant="contained"
+                                sx={{ color: '#808080', bgcolor: 'white',
+                                    ':hover': { bgcolor: 'rgba(234,229,229,0.38)' } }}
+                                onClick={handleReset}
+                        >
+                            Clear
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+
+            <Button onClick={showDataset}>View DataSet</Button>
+            <Dataset dataset={dataset}/>
 
             <Footer/>
         </div>
