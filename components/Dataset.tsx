57dc722cd0eddb42ae41cf45e8a120ce8e216f06
efesoa <molingeefesoa@gmail.com>
2022-07-22 18:11:08 +0200

    Adding a component for table

--- components/Dataset.tsx
+++ components/Dataset.tsx
@@ -1,86 +1,12 @@
-import Paper from "@mui/material/Paper";
-import React, {useState} from "react";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from "@mui/material";
+import React from "react";
+import TableDisplay from "./TableDisplay";
 
-interface Column {
-    id: 'sepal_length' | 'sepal_width' | 'petal_length' | 'petal_width' | 'specie';
-    label: string;
-    align?: 'right';
-    minWidth?: number;
-}
+export default function Dataset(props: { dataset: object[] | null; }) {
 
-const columns: readonly Column[] = [
-    { id: 'sepal_length', label: 'Sepal Length' },
-    { id: 'sepal_width', label: 'Sepal Width' },
-    { id: 'petal_length', label: 'Petal Length' },
-    { id: 'petal_width', label: 'Petal Width' },
-    { id: 'specie', label: 'Specie' }
-]
-
-export default function Dataset(props) {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
-    const irisDataset = (dataset) => {
+    const irisDataset = (dataset: object[] | null) => {
         if (dataset != null) {
             return (
-                <Paper sx={{width: '100%', overflow: 'hidden'}}>
-                    <TableContainer sx={{maxHeight: 440}}>
-                        <Table stickyHeader aria-label="sticky table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell sx={{ bgcolor: 'lightgray' }}>SN</TableCell>
-                                    {columns.map((column) => (
-                                        <TableCell
-                                            key={column.id}
-                                            align={column.align}
-                                            style={{minWidth: column.minWidth}}
-                                            sx={{ bgcolor: 'lightgray' }}
-                                        >
-                                            {column.label}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {dataset
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((flower, index) => (
-                                            <TableRow key={flower.name} hover sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-                                                <TableCell>{index + 1}</TableCell>
-                                                {columns.map((column) => {
-                                                    const value = flower[column.id];
-                                                    return (
-                                                        <TableCell key={column.id} align={column.align}>
-                                                            {value}
-                                                        </TableCell>
-                                                    );
-                                                })}
-                                            </TableRow>
-                                    ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        sx={{ bgcolor: 'lightgray' }}
-                        rowsPerPageOptions={[10, 30, 50, 100]}
-                        component="div"
-                        count={dataset.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                </Paper>
+                <TableDisplay dataset={dataset}/>
             );
         } else {
             return(<p></p>)
--- components/KSimilarObjectResult.tsx
+++ components/KSimilarObjectResult.tsx
@@ -1,7 +1,10 @@
-import {Box, Grid} from "@mui/material";
+import {Box, Grid, Typography} from "@mui/material";
+import React from "react";
+import TableDisplay from "./TableDisplay";
 
-export default function KSimilarObjectResult(prop) {
-    const KSimilar = (compute) => {
+export default function KSimilarObjectResult(prop: { compute: any; }) {
+
+    const KSimilar = (compute: any[] | null) => {
         if (compute != null) {
             return(
                 <><br/>
@@ -22,37 +25,10 @@ export default function KSimilarObjectResult(prop) {
                             )}
 
                         if (index == 1) {
-                            return (<>
-                                {com.map(m => {
-                                    return(
-                                        <p>{m}</p>
-                                    )
-                                    })}</>
-                                // <Box boxShadow={2} sx={{ p:2, borderLeftColor: 'orange', borderRightColor: 'orange' }} borderLeft={3} borderRight={3} borderRadius={2}>
-                                //     <Grid container spacing={2}
-                                //           direction="row"
-                                //           justifyContent="center"
-                                //           alignItems="center"
-                                //     >
-                                //         <Grid item>
-                                //             Predicting Second Flower: <b>{com}</b>
-                                //         </Grid>
-                                //     </Grid>
-                                // </Box>
-                            )}
-
-                        if (index == 2) {
                             return (
                                 <Box boxShadow={2} sx={{ p:2, borderLeftColor: 'orange', borderRightColor: 'orange' }} borderLeft={3} borderRight={3} borderRadius={2}>
-                                    <Grid container spacing={2}
-                                          direction="row"
-                                          justifyContent="center"
-                                          alignItems="center"
-                                    >
-                                        <Grid item>
-                                            Cosine Similarity: <b>{com}</b>
-                                        </Grid>
-                                    </Grid>
+                                    <Typography><b>K-Nearest Flower(s)</b></Typography>
+                                    <TableDisplay dataset={com}/>
                                 </Box>
                             )}
                     })}<br/></>
