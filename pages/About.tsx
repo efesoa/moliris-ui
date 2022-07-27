d9d4ce55b21016ccc998de667920de7384b70a48
efesoa <molingeefesoa@gmail.com>
2022-07-27 13:47:00 +0200

    Update

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import * as React from "react";
 
 export default function About() {
     return (
         <div>
             <Navbar/>
             <h1>About</h1>
+
+            <Footer />
         </div>
     )
 }
