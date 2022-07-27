a1477a67729678042eb0db843c63358422b815c5
efesoa <molingeefesoa@gmail.com>
2022-07-27 13:47:10 +0200

    Update

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -7,7 +7,7 @@ import { Button, CardActionArea, CardActions, CardMedia } from '@mui/material';
 export default function Cards(props: { image: string; name: string; description: string; }) {
     const {image, name, description} = props
     return (
-        <Card sx={{ minWidth: 300, maxWidth: 345, borderRadius: 10, borderStartStartRadius: 10, borderEndEndRadius: 10 }}>
+        <Card sx={{ minWidth: 300, maxWidth: 315, borderRadius: 10, borderStartStartRadius: 10, borderEndEndRadius: 10 }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
@@ -26,7 +26,7 @@ export default function Cards(props: { image: string; name: string; description:
             </CardActionArea>
             <CardActions>
                 <Button size="small" color="primary">
-                    Share
+                    More
                 </Button>
             </CardActions>
         </Card>
