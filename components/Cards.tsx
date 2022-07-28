f186c5af1b38a5603caa13e40d949ccea57d80cb
efesoa <molingeefesoa@gmail.com>
2022-07-28 20:16:45 +0200

    Removing card action area

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,28 +2,26 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, CardMedia } from '@mui/material';
+import { Button, CardActions, CardMedia } from '@mui/material';
 
 export default function Cards(props: { image: string; name: string; description: string; }) {
     const {image, name, description} = props
     return (
         <Card sx={{ minWidth: 300, maxWidth: 315, borderRadius: 10, borderStartStartRadius: 10, borderEndEndRadius: 10 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    src={image}
-                    alt="iris flower"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {description}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+            <CardMedia
+                component="img"
+                height="140"
+                src={image}
+                alt="iris flower"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
             <CardActions>
                 <Button size="small" color="primary">
                     More
