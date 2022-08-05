1a9eae31ee36f34687405c6c4756a4fc72e9b9b0
efesoa <molingeefesoa@gmail.com>
2022-08-05 21:33:13 +0200

    Creating Benefits box commponent

---
+++ components/BenefitBox.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {Box, CardMedia, Typography} from "@mui/material";
+
+
+export default function BenefitBox(props: { image: string; name: string; description: string; }) {
+    const {image, name, description} = props
+    return (
+        <Box boxShadow={3} sx={{ p:2, maxWidth: 250, background: 'white' }} borderRadius={3}>
+            <CardMedia
+                image={image}
+                style={{height: 120, borderRadius: 5}} />
+            <Typography gutterBottom variant="h5" component="div">
+                {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {description}
+            </Typography>
+        </Box>
+    )
+}
