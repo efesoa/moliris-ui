315d7d5bf5b13fc3558509beefd3c4591815497c
efesoa <molingeefesoa@gmail.com>
2022-08-27 19:07:10 -0200

    Making box component more flexible

--- components/BenefitBox.tsx
+++ components/BenefitBox.tsx
@@ -2,17 +2,19 @@ import * as React from 'react';
 import {Box, CardMedia, Typography} from "@mui/material";
 
 
-export default function BenefitBox(props: { image: string; name: string; description: string; }) {
-    const {image, name, description} = props
+export default function BenefitBox(props: { width: number, height: number, border: number, image: string; name: string; description: string; }) {
+    const {width, height, border, image, name, description} = props
     return (
-        <Box boxShadow={3} sx={{ p:2, maxWidth: 250, minHeight: 300, background: 'white' }} borderRadius={3}>
+        <Box sx={{ p:2, maxWidth: {width}, minHeight: {height}, borderColor: '#603BE2', borderRadius: 4,
+            color: '#603BE2', align: 'center'}} border={border} >
             <CardMedia
                 image={image}
-                style={{height: 120, borderRadius: 5}} />
-            <Typography gutterBottom variant="h5" component="div">
+                style={{height: 90}}
+            />
+            <Typography gutterBottom variant="h5" component="div" align="center">
                 {name}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="text.secondary" align="center">
                 {description}
             </Typography>
         </Box>
