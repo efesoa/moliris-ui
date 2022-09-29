6c2c323c884b6bfb69432197a6f0340ca5e2d549
efesoa <molingeefesoa@gmail.com>
2022-09-29 14:41:40 +0200

    .

--- pages/About.tsx
+++ pages/About.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 export default function About() {
     return (
         <div>
-            <Navbar  children={1}/>
+            <Navbar>children={1}</Navbar>
             <h1>About</h1>
 
             <Footer />
