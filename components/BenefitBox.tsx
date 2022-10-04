dfe93db5932267bc711b04801210454bb3f20c25
efesoa <molingeefesoa@gmail.com>
2022-10-04 19:58:11 +0200

    Updating component border

--- components/BenefitBox.tsx
+++ components/BenefitBox.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import {Box, CardMedia, Typography} from "@mui/material";
 
 
-export default function BenefitBox(props: { width: number, height: number, border: number, image: string; name: string; description: string; }) {
-    const {width, height, border, image, name, description} = props
+export default function BenefitBox(props: { width: number, height: number, border: number, image: string; name: string;
+    description: string; bLeftRadius: number; bRightRadius: number; tLeftRadius: number; tRightRadius: number;}) {
+    const {width, height, border, image, name, description, bLeftRadius, bRightRadius, tLeftRadius, tRightRadius} = props
     return (
-        <Box sx={{ p:2, maxWidth: {width}, minHeight: {height}, borderColor: '#603BE2', borderRadius: 4,
-            color: '#603BE2', align: 'center'}} border={border} >
+        <Box sx={{ p:2, maxWidth: {width}, minHeight: {height}, borderColor: '#603BE2', color: '#603BE2',
+            align: 'center', borderBottomLeftRadius: bLeftRadius, borderBottomRightRadius: bRightRadius,
+            borderTopLeftRadius: tLeftRadius, borderTopRightRadius: tRightRadius }} border={border} >
             <CardMedia
                 image={image}
                 style={{height: 90}}
