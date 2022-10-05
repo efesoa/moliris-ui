a00c6e0af136a02bc644574e74598c7239103285
efesoa <molingeefesoa@gmail.com>
2022-10-05 17:11:20 +0200

    carousel slider image update

--- components/CarouselSlider.tsx
+++ components/CarouselSlider.tsx
@@ -6,24 +6,24 @@ export default function CarouselSlider()
 {
     const items = [
         {
-            image: '/static/images/collect.png',
+            image: '/static/images/bearded.png',
             name: "Bearded Iris",
             description: "Bearded Iris (iris germanica) is an evergreen perennial rhizome found in well drained soil from Southern and Central Europe."
         },
         {
-            image: '/static/images/compute.png',
+            image: '/static/images/aril.png',
             name: "Aril Iris",
             description: "Arils, or aril irises are wild bearded iris species found in semi-arid to desert climates from Central Asia to the Middle East."
         },
         {
-            image: '/static/images/result.png',
+            image: '/static/images/beardless.png',
             name: "Beardless Iris",
             description: "Beardless Iris without a beard found from the Southern areas of the template zone up to the edges of the frigid arctic."
         }
     ];
 
     return (
-        <Grid sx={{ paddingTop: 2, paddingBottom: 2, width: '100%', backgroundColor: '#F3F0F0'
+        <Grid sx={{ paddingTop: 5, paddingBottom: 3, width: '100%', backgroundColor: '#F5F5F5'
         }}>
         <Carousel fullHeightHover={true} navButtonsProps={{ style: { backgroundColor: '#603BE2', borderRadius: 7 } }}
                   indicatorIconButtonProps={{ style: { color: '#A890FF' } }}
@@ -45,7 +45,7 @@ function Item(props: { item: { image: string | undefined; name: string | number 
               justifyContent="center"
               alignItems="center"
         >
-            <Typography variant={'h5'} sx={{ color: '#603BE2' }}>IRIS CATEGORIES</Typography>
+            <Typography variant={'h5'} sx={{ color: '#000000' }}>IRIS CATEGORIES</Typography>
             <Grid container
                   direction="row"
                   justifyContent="space-evenly"
@@ -53,9 +53,14 @@ function Item(props: { item: { image: string | undefined; name: string | number 
                   sx={{ paddingTop: 3, px: 4 }}
             >
                 <Grid item>
-                    <img alt={'Iris Categories'} src={props.item.image} style={{height: 200}}/>
+                    <Grid sx={{ display: { xs: 'inherit', sm: 'inherit', md: 'none', lg: 'none' } }}>
+                        <img alt={'Iris Categories'} src={props.item.image} style={{height: 350}}/>
+                    </Grid>
+                    <Grid sx={{ display: { xs: 'none', sm: 'none', md: 'inherit', lg: 'inherit' } }}>
+                        <img alt={'Iris Categories'} src={props.item.image} style={{height: 400}}/>
+                    </Grid>
                 </Grid>
-                <Grid item>
+                <Grid item sx={{ paddingTop: 2 }}>
                     <Typography variant={'h5'} sx={{ paddingBottom: 1, color: '#603BE2' }}>{props.item.name}</Typography>
                     <Box maxWidth={500}>
                         <Typography>{props.item.description}</Typography>
