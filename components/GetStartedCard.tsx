8d6992823cce399199dae3550af5b05b72f3b63a
efesoa <molingeefesoa@gmail.com>
2022-10-07 15:21:15 +0200

    updating card

--- components/GetStartedCard.tsx
+++ components/GetStartedCard.tsx
@@ -2,24 +2,27 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import {CardActionArea, CardActions, CardMedia} from '@mui/material';
+import {CardActionArea, CardMedia} from '@mui/material';
+import Link from "next/link";
 
-export default function GetStartedCard(props: { image: string; name: string; }) {
-    const {image, name} = props
+export default function GetStartedCard(props: { image: string; name: string; go: string }) {
+    const {image, name, go} = props
     return (
-        <Card sx={{ minWidth: 300, borderRadius: 10, borderStartStartRadius: 10, borderEndEndRadius: 10 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    src={image}
-                    alt={name}
-                    sx={{ minWidth: 300, minHeight: 200, borderRadius: 10, borderStartStartRadius: 10, borderEndEndRadius: 10 }}
-                />
-                <CardContent >
-                    <Typography gutterBottom variant="h6" align="center">{name}</Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <Link href={go}>
+            <Card sx={{ minWidth: 300, borderRadius: 8 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        src={image}
+                        alt={name}
+                        sx={{ px: 1.5,paddingTop: 1.5, minWidth: 300, minHeight: 200, borderRadius: 8 }}
+                    />
+                    <CardContent >
+                        <Typography gutterBottom variant="h6" align="center">{name}</Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
     );
 }
