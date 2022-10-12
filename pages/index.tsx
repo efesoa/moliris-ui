4d85b6a73a07281933d32b64422a17a8ebfa9f6f
efesoa <molingeefesoa@gmail.com>
2022-10-12 19:47:40 +0200

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import styles from '../styles/Home.module.css'
 import Navbar from "../components/Navbar";
 import * as React from "react";
 import Footer from "../components/Footer";
-import Cards from "../components/Cards"
 import {Button, Grid, Typography} from "@mui/material";
 import BenefitBox from "../components/BenefitBox";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
@@ -329,55 +328,6 @@ const Home: NextPage = () => {
           </Grid>
         </Grid>
 
-        {/*<Typography variant="h4"><b>Iris Categories</b></Typography>*/}
-        {/*<Grid container*/}
-        {/*  direction="row"*/}
-        {/*  justifyContent="center"*/}
-        {/*  alignItems="center"*/}
-        {/*>*/}
-        {/*  {specieType.map(spec => {*/}
-        {/*    return (*/}
-        {/*          <Grid item sx={{ p: 1 }}>*/}
-        {/*            <Cards image={spec.image} name={spec.name} description={spec.description} />*/}
-        {/*          </Grid>*/}
-        {/*    )*/}
-        {/*  })}*/}
-        {/*</Grid>*/}
-
-        {/*<p className={styles.description}>*/}
-        {/*  Get started by editing{' '}*/}
-        {/*  <code className={styles.code}>pages/index.tsx</code>*/}
-        {/*</p>*/}
-
-        {/*<div className={styles.grid}>*/}
-        {/*  <a href="https://nextjs.org/docs" className={styles.card}>*/}
-        {/*    <h2>Documentation &rarr;</h2>*/}
-        {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a href="https://nextjs.org/learn" className={styles.card}>*/}
-        {/*    <h2>Learn &rarr;</h2>*/}
-        {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://github.com/vercel/next.js/tree/canary/examples"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Examples &rarr;</h2>*/}
-        {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Deploy &rarr;</h2>*/}
-        {/*    <p>*/}
-        {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
       </main>
 
       <Footer />
