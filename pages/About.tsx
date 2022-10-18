ceefdbfcf8927679686a4fd57f329985d843aa98
efesoa <molingeefesoa@gmail.com>
2022-10-18 22:48:23 +0200

    about us description

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,7 +1,9 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import * as React from "react";
+import styles from '../styles/Home.module.css'
 import {Grid, Typography} from "@mui/material";
+import Image from "next/image";
 
 export default function About() {
     return (
@@ -15,6 +17,22 @@ export default function About() {
             >
                 <Grid item>
                     <Typography variant="h2" sx={{ color: '#ffffff' }}>ABOUT US</Typography>
+                </Grid>
+            </Grid>
+            <Grid container
+                  direction="row"
+                  justifyContent="space-evenly"
+                  alignItems="flex-start"
+                  sx={{ py: 3 }}
+            >
+                <Grid item>
+                    <Image src={'/static/images/about-icons.png'} width={200} height={400} />
+                </Grid>
+                <Grid item sx={{ width: 550 }} className={styles.container}>
+                    <Typography variant="h4"><b>Computing Object-Object Similarities using data points.</b></Typography><br/>
+                    <Typography>We quantify the similarity between two objects using similarity metric base on UCI Machine
+                        Learning dataset. Get real value predictions of data points using powerful Machine Learning algorithms.
+                        Filter similar data points for easy access.</Typography>
                 </Grid>
             </Grid>
 
